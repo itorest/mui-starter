e55ebdf0e9a780090d7dbd4dcc3fc9c6da5e22e3
itorest <78473381+itorest@users.noreply.github.com>
2021-12-16 12:48:36 +0800

    feat: opt

--- src/theme.ts
+++ src/theme.ts
@@ -1,11 +1,11 @@
 import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { blue, red } from '@mui/material/colors';
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: blue.A700,
     },
     secondary: {
       main: '#19857b',
